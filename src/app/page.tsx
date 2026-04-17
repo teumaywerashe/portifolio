5e5f3b2a7694ad67bbd812c27816838c4fd2f201
teumaywerashe <teumay21@gmail.com>
2026-04-17 18:30:48 +0300

    feat: add About, Projects, Skills, and ContactForm components to HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,21 @@
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import { Contact } from "lucide-react";
+import ContactForm from "@/components/ContactForm";
 
 export default function HomePage() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <About/>
+      <Projects/>
+      <Skills/>
+      <ContactForm/>
       <Footer />
     </div>
   );
